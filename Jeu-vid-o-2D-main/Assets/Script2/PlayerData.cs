6479a9262ae8856358fdff715d90b180172e2533
Badiane95 <165165737+Badiane95@users.noreply.github.com>
2025-03-11 23:49:03 +0100

    save

--- Jeu-vid-o-2D-main/Assets/Script2/PlayerData.cs
+++ Jeu-vid-o-2D-main/Assets/Script2/PlayerData.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PlayerData", menuName = "Scriptable Objects/Player Data")]
+[CreateAssetMenu(fileName = "PlayerData", menuName = "Scriptable Objects/Variables/PlayerData")]
 public class PlayerData : ScriptableObject
 {
-   public int currentLifePoints = 0; 
-   public int maxLifePoints = 3; 
+    public int currentLifePoints = 0;
+    public int maxLifePoints = 3;
 
-   public void TakeDamage(int damage)
-   {
-       currentLifePoints = Mathf.Max(0, currentLifePoints - damage);
-   }
+    // Méthode pour infliger des dégâts au joueur
+    public void TakeDamage(int damage)
+    {
+        // Réduction des points de vie actuels
+        currentLifePoints -= damage;
+
+        // S'assurer que les points de vie ne descendent pas en dessous de zéro
+        currentLifePoints = Mathf.Max(currentLifePoints, 0);
+
+        Debug.Log($"Le joueur a pris {damage} dégâts. Points de vie restants : {currentLifePoints}");
+    }
 }
--- Jeu-vid-o-2D-main/Assets/Scripts/CurrentScene.cs
+++ Jeu-vid-o-2D-main/Assets/Scripts/CurrentScene.cs
@@ -1,72 +1,63 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CurrentScene : MonoBehaviour
+public class CurrentSceneManager : MonoBehaviour
 {
-    private bool isPaused = false;
-
-    public GameObject game;
-    
-    public PauseMenu pause;
-
+    public GameObject gameOverScreen;
+    public GameObject playerMovement;
+    public bool isPaused = false;
+    public GameObject pauseScreen; 
     public VoidEventChannel onPlayerDeath;
+    public VoidEventChannel onPause;
+    public VoidEventChannel onResume;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
+    }
 
     private void OnEnable()
     {
-        if (onPlayerDeath != null)
-        {
-            onPlayerDeath.OnEventRaised += Die;
-        }
+        onPlayerDeath.OnEventRaised += Die;    
     }
 
     private void OnDisable()
     {
-        if (onPlayerDeath != null)
-        {
-            onPlayerDeath.OnEventRaised -= Die;
-        }
+        onPlayerDeath.OnEventRaised -= Die;        
     }
 
-    private void Die()
-    {
-        game.SetActive(true);
-        // Implement game over logic here
-        Debug.Log("Player died");
+    private void Die() {
+        gameOverScreen.SetActive(true);
     }
 
+    public void Pause() {
+        if (Time.timeScale == 0) {
+                Time.timeScale = 1;
+                isPaused = false;
+                pauseScreen.SetActive(false);
+                onResume.Raise();
+            } else {
+                Time.timeScale = 0;
+                isPaused = true;
+                pauseScreen.SetActive(true);
+                onPause.Raise();
+            }
+    }
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            TogglePause();
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Pause();
         }
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            RestartScene();
+#if UNITY_EDITOR 
+        if(Input.GetKeyDown(KeyCode.R)) {
+            RestartGame();
         }
+#endif
     }
 
-    void TogglePause()
-    {
-        isPaused = !isPaused;
-        Time.timeScale = isPaused ? 0 : 1;
-        Debug.Log(isPaused ? "Game Paused" : "Game Resumed");
-    }
-
-    public void RestartScene()
-    {
-        Time.timeScale = 1; // Ensure the game is not paused when restarting
+    public void RestartGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        Debug.Log("Scene Restarted");
     }
-
-    void Start()
-    {
-        game.SetActive(false);
-    }
-
-
 }
-
-
